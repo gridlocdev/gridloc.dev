dfb0e00a8ee858e07571d6fe17f010777a0ec553
gridlocdev <gridlocdev@gmail.com>
2021-08-28 13:55:14 -0700

    Added call to action buttons, revised theming, and normalized sass imports

--- app/index.js
+++ app/index.js
@@ -1,15 +1,30 @@
 function toggleTheme() {
-    var rootElement = document.getElementsByTagName('html')[0]
+    let rootElement = document.getElementsByTagName('html')[0]
     const themeStatus = rootElement.getAttribute('data-theme')
+
+    let darkThemeToggleButtons = document.querySelectorAll('.dark-theme-toggle')
+
     switch (themeStatus) {
         case 'light':
             rootElement.setAttribute('data-theme', 'dark')
+            darkThemeToggleButtons.forEach(element => {
+                element.setAttribute('src', 'assets/images/dark_mode_black_24dp.svg')
+                element.src = './assets/images/light_mode_black_24dp.svg'
+            })
             break
         case 'dark':
             rootElement.setAttribute('data-theme', 'light')
+            darkThemeToggleButtons.forEach(element => {
+                element.setAttribute('src', 'assets/images/light_mode_black_24dp.svg')
+                element.src = './assets/images/light_mode_black_24dp.svg'
+            })
             break
         default:
             rootElement.setAttribute('data-theme', 'light')
+            darkThemeToggleButtons.forEach(element => {
+                element.src = 'assets/images/light_mode_black_24dp.svg'
+                element.setAttribute('src', 'assets/images/light_mode_black_24dp.svg')
+            })
             break
     }
 }
