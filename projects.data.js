ad2907035a299d870c7009390e757c6f885ba0b3
gridlocdev <gridlocdev@gmail.com>
2021-09-08 13:24:07 -0700

    Added scroll navigation function

--- projects.data.js
+++ projects.data.js
@@ -3,7 +3,7 @@ export default [
         "name": "Text Adventure",
         "description": "Create, play, import or export a text adventure game story.",
         "technologies": "Vue 2, Vuetify, VueX",
-        "bootstrap_icon": "bi-textarea-t",
+        "bootstrapIcon": "bi-textarea-t",
         "links": {
             "repository": "https://github.com/gridlocdev/text-adventure",
             "website": "https://gridlocdev.github.io/text-adventure"
@@ -13,7 +13,7 @@ export default [
         "name": "Drum Machine",
         "description": "A drum machine that you can play with the keyboard.",
         "technologies": "Vue 2, Vuetify",
-        "bootstrap_icon": "bi-music-player",
+        "bootstrapIcon": "bi-music-player",
         "links": {
             "repository": "https://github.com/gridlocdev/drum-machine",
             "website": "https://gridlocdev.github.io/drum-machine"
@@ -23,7 +23,7 @@ export default [
         "name": "Chess Clock",
         "description": "A web-based clock for timing games of Chess.",
         "technologies": "Angular, Angular Material",
-        "bootstrap_icon": "bi-stopwatch",
+        "bootstrapIcon": "bi-stopwatch",
         "links": {
             "repository": "https://github.com/gridlocdev/chess-clock",
             "website": "https://gridlocdev.github.io/chess-clock"
@@ -33,7 +33,7 @@ export default [
         "name": "Portfolio (old)",
         "description": "My old portfolio website, when I was just getting started with web development. It contains a similar set of links to my projects to this website!",
         "technologies": "Vue 2, Bootstrap-vue",
-        "bootstrap_icon": "bi-map",
+        "bootstrapIcon": "bi-map",
         "links": {
             "repository": "https://github.com/gridlocdev/portfolio",
             "website": "https://gridlocdev.github.io/portfolio"
@@ -43,7 +43,7 @@ export default [
         "name": "Cube Slide",
         "description": "A platformer video game with no jumping, where a cube slides around obstacles to make its way to the goal.",
         "technologies": "Unity (Game Engine)",
-        "bootstrap_icon": "bi-box",
+        "bootstrapIcon": "bi-box",
         "links": {
             "repository": "https://github.com/gridlocdev/cube-slide",
             "website": "https://gridlocdev.github.io/cube-slide"
