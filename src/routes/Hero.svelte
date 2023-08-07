fe49f7b42f523be0ee8686283b1da897462a76eb
gridlocdev <gridlocdev@fastmail.com>
2023-08-07 16:33:40 -0700

    Improved legibility of text, background, and buttons

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -3,7 +3,7 @@
 	<p class="mb-12 text-2xl opacity-80">I've made some stuff, feel free to check it out</p>
 	<a
 		href="#projects"
-		class="m-auto cursor-pointer rounded-full bg-accent px-10 py-3 text-2xl font-bold transition hover:scale-95 hover:bg-opacity-50"
+		class="bg-transparent m-auto cursor-pointer rounded-full border-2 border-accent px-10 py-3 text-2xl font-bold transition hover:scale-95 hover:bg-opacity-50 hover:opacity-75"
 	>
 		sounds cool
 	</a>
--- src/routes/ProjectListItem.svelte
+++ src/routes/ProjectListItem.svelte
@@ -8,19 +8,37 @@
 	export let projectType;
 </script>
 
-<article class="flex flex-col gap-5 rounded-lg bg-text/[.1] p-10 shadow-2xl">
-	<h2 class="text-2xl font-bold">{title}</h2>
-	<p class="text-xl">{description}</p>
-	{#if screenshot}
-		<img src={screenshot} class="w-full" alt="(screenshot of {title})" />
-	{/if}
-	<span class="italic opacity-75">Built with: {technologies}</span>
-	<div class="mt-auto flex justify-around">
+<article class="flex flex-col justify-between gap-2 rounded-lg bg-text/[.1] p-10 shadow-2xl">
+	<div class="flex flex-col gap-2">
+		<h2 class="text-2xl font-bold">{title}</h2>
+		<p class="text-xl">{description}</p>
+	</div>
+	<div class="flex flex-col gap-2">
+		{#if screenshot}
+			<img src={screenshot} class="w-full" alt="(screenshot of {title})" />
+		{/if}
+		<span class="italic opacity-75">Built with: {technologies}</span>
+	</div>
+	<div class="flex place-content-around gap-5">
+		{#if source}
+			<a
+				href={source}
+				target="_blank"
+				class="bg-transparent flex w-fit cursor-pointer gap-3 rounded-full border-2 border-accent px-9 py-3 text-xl font-bold transition hover:scale-95 hover:opacity-60"
+			>
+				<span>View Source</span>
+				<img
+					src="/icons/github-mark.svg"
+					alt="GitHub icon"
+					class="inline h-7 w-7 justify-self-center"
+				/>
+			</a>
+		{/if}
 		{#if website}
 			<a
 				href={website}
 				target="_blank"
-				class="flex cursor-pointer gap-2 rounded-full bg-accent px-9 py-3 text-xl transition hover:scale-95 hover:bg-opacity-50"
+				class="flex w-fit cursor-pointer items-center gap-3 rounded-full bg-accent bg-gradient-to-tl from-secondary px-9 py-3 text-xl font-bold brightness-125 transition hover:scale-95 hover:brightness-100"
 			>
 				{#if projectType === 'App'}
 					<span>Open App</span>
@@ -32,16 +50,6 @@
 					<span>Play</span>
 					<img src="/icons/play.svg" alt="play icon" class="h-7 w-7" />
 				{/if}
-			</a>
-		{/if}
-		{#if source}
-			<a
-				href={source}
-				target="_blank"
-				class="flex cursor-pointer gap-3 rounded-full bg-accent px-9 py-3 text-xl transition hover:scale-95 hover:bg-opacity-50"
-			>
-				<span>View Source</span>
-				<img src="/icons/github-mark-white.svg" alt="GitHub icon" class="inline h-7 w-7" />
 			</a>
 		{/if}
 	</div>
