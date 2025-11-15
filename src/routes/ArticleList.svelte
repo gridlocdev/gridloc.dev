b22980765e6fff2c22df725357146f4bdba5bdea
gridlocdev <gridlocdev@fastmail.com>
2025-11-15 10:52:40 -0700

    remove developer notes
    It's still useful to me, but I only access it on the github page anyway now

--- src/routes/ArticleList.svelte
+++ src/routes/ArticleList.svelte
@@ -5,11 +5,6 @@
 <h2 id="articles" class="mt-12 p-12 text-center text-4xl font-bold">Articles</h2>
 <div class="mx-10 grid grid-cols-1 gap-8 md:text-start lg:grid-cols-3">
 	<ArticleCard
-		title="Developer Notes"
-		description="Notes about miscellaneous industry-related topics"
-		website="https://notes.gridloc.dev"
-	/>
-	<ArticleCard
 		title="Blog"
 		description="A website where I put short-form guides for things I can't find on the Internet"
 		website="https://listed.to/@gridlocdev"
